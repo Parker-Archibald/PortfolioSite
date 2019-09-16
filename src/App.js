8ef1c9688645590e0086191f116fe29b9555dda2
Parker Archibald <parkerarchibald97@gmail.com>
2019-09-16 13:30:19 -0600

    test4

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div>
     <div className="App">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <Router>
         <NavBar/>
         <Routes/>
